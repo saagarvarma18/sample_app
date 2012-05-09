57feb942d27b50220a06c063e673cc3327266c34
saagar <saagarvarma18@gmail.com>
2012-05-09 12:13:17 +0530

    Helper +CSS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+
+#Return the title in case it has not yet
+
+   def title 
+ 	base_title="Ruby on Rails Application"
+  	if @title.nil?
+	   base_title
+	else
+	   "#{base_title}|#{@title}"
+	end
+   end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,9 @@ require 'spec_helper'
 describe PagesController do
  render_views
 
+   before(:each) do
+	@base_title="Ruby on Rails Application"
+   end
 
   describe "GET 'home'" do
     it "returns http success" do
@@ -12,7 +15,7 @@ describe PagesController do
  
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title", :content => "Ruby on Rails Application|Home")	 
+      response.should have_selector("title",:content => "#{@base_title}|Home")	 
     end 
 
     it "should have a non-blank body" do
@@ -29,7 +32,7 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title", :content => "Ruby on Rails Application|Contact")	 
+      response.should have_selector("title", :content => "#{@base_title}|Contact")	 
     end 
 
 
@@ -42,7 +45,7 @@ describe PagesController do
     end
    it "should have the right title" do
       get 'about'
-      response.should have_selector("title", :content => "Ruby on Rails Application|About")	 
+      response.should have_selector("title", :content => "#{@base_title}|About")	 
     end 
 
   end
