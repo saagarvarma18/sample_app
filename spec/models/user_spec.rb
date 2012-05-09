0514c409a9a863c7111db65780099575b034fc66
saagar <saagarvarma18@gmail.com>
2012-05-09 20:09:28 +0530

    Lesson 7

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,12 @@ require 'spec_helper'
 describe User do
   
   before (:each) do
-  	@attr= { :name => "Example User", :email => "user@example.com"}
+  	@attr= { 
+  		:name => "Example User", 
+  		:email => "user@example.com",
+  		:password => "foobar",
+  		:password_confirmation => "foobar"
+  	}
   end
 
   it "should create a new instance given a valid attribute" do
