a02d306df82bb7bcab592faf0654dd2841fc2450
saagar <saagarvarma18@gmail.com>
2012-05-10 15:38:03 +0530

    Creating invalid signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
   def new
+  	@title="Sign In"
   end
+
+  def create
+  	user=User.authenticate(params[:session][:email], params[:session][:password])
+
+  	if user.nil?
+  		flash.now[:error] = "Invalid email/Passwprd combination"
+  		@title="Sign In"
+  		render 'new'  	
+  	else
+  		#Handle it
+  	end
+  end
+
+  def destroy
+  	
+  end
+
 end
