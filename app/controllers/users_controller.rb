08f053edd29b2fb214b976f62ee73f44af476076
Saagar <saagarvarma18@gmail.com>
2012-05-11 23:34:23 +0530

    Deletin working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   before_filter :authenticate, :only => [:index,:edit, :update]
   before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user,:only => :destroy
+
 
   def index
     @users = User.paginate(:page => params[:page])
@@ -18,7 +20,12 @@ class UsersController < ApplicationController
   	@title='Sign Up'
   end
 
-  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
+
 
   def create
   	@user=User.new(params[:user])
@@ -56,4 +63,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     redirect_to root_path unless current_user?(@user)
   end  
+
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+  end
+
 end
